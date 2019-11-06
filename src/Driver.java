12b83337014a4bcf9c049efe56bfcd9231d813f9
P1326570 <P1326570@NCS-220819KK06.ncs.corp.int-ads>
2019-11-06 14:15:53 +0800

    user menu added

---
+++ src/Driver.java
@@ -0,0 +1,37 @@
+
+public class Driver {
+	
+	Task task;
+	ToDoList list = new ToDoList();
+	public static void main(String[] args) {
+		Driver d = new Driver();
+		d.userMenu();
+	}
+	
+	public void userMenu() {
+		System.out.println("===============================");
+		System.out.println("1. Add Task");
+		System.out.println("2. Remove Task");
+		System.out.println("3. Edit Task");
+		System.out.println("4. View Task");
+		int option = Keyboard.readInt("Enter option:");
+		userInput(option);
+	}
+	
+	public void userInput(int option) {
+
+		switch(option) {
+			case 1: {
+				String desc = Keyboard.readString("Enter Task Description: ");
+				String completed = Keyboard.readString("Is Task completed? (Y/N): ");
+				
+				if(completed.equalsIgnoreCase("Y"))
+					task = new Task(desc, true);
+				else
+					task = new Task(desc, false);
+				
+				list.addTask(task);	
+			}
+		}
+	}
+}
